efc3e2834f9f6cc4933643cf2f2817fd3291a763
bcharlotin1 <biancacharlotin1@gmail.com>
2021-09-24 15:16:55 -0400

    commit tree 063520e783b00c3f324f3d2e3a481b34994f0a59

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+class Polygon{
+    constructor(array){
+        this.array = array
+    }
+
+    get countSides(){
+        return this.array.length 
+    }
+
+    get perimeter(){
+        return this.array.reduce()
+    }
+
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+        if (Polygon.countSides === 3){
+
+            let a = Polygon.countSides[0]
+            let b = Polygon.countSides[1]
+            let c = Polygon.countSides[2]
+
+            if (a + b <= c || a + c <= b || b + c <= a) {
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false
+        }
+    }
+}
